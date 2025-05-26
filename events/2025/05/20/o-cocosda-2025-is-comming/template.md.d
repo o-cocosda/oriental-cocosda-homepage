cb866dcc254d6072a385606857deb15195c3951a
huonwe <huonwe@users.noreply.github.com>
2025-05-26 04:15:12 +0000

    Deploy Hugo site to public branch [skip ci] c13ce87b77cca9dffd7fefbae142c72ce0a0c8f2

---
+++ events/2025/05/20/o-cocosda-2025-is-comming/template.md.d
@@ -0,0 +1,47 @@
+---
+date: 2025-05-20
+title: O-COCOSDA 2025 is comming!
+linkTitle: O-COCOSDA 2025
+description: >
+  12–14 November 2025, Universitas Kristen Duta Wacana (UKDW), Yogyakarta, Indonesia
+# author: Riona MacNamara ([@rionam](https://x.com/bepsays))
+resources:
+  - src: "**.{png,jpg}"
+    title: "Image #:counter"
+    params:
+      byline: "Universitas Kristen Duta Wacana – Conference Venue"
+      # byline: "Photo: Riona MacNamara / CC-BY-CA"
+---
+
+<!-- **This is a typical blog post that includes images.**
+
+The 28th International Conference of Oriental COCOSDA will take place in person from November 12–14, 2025, at Universitas Kristen Duta Wacana (UKDW), Yogyakarta, Indonesia.
+We strongly encourage attendees to join physically to fully engage with and experience the event.
+
+## Including images
+
+Here's an image (`featured-sunset-get.png`) that includes a byline and a caption.
+
+{{< imgproc sunset Fill "600x300" >}}
+Fetch and scale an image in the upcoming Hugo 0.43.
+{{< /imgproc >}}
+
+The front matter of this post specifies properties to be assigned to all image resources:
+
+```
+resources:
+- src: "**.{png,jpg}"
+  title: "Image #:counter"
+  params:
+    byline: "Photo: Riona MacNamara / CC-BY-CA"
+```
+
+To include the image in a page, specify its details like this:
+
+```go-html-template
+{{</* imgproc sunset Fill "600x300" */>}}
+Fetch and scale an image in the upcoming Hugo 0.43.
+{{</* /imgproc */>}}
+```
+
+The image will be rendered at the size and byline specified in the front matter. -->
